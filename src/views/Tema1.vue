11782b12171acb799b17e442d8c8532b0d827c86
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-09-24 08:09:12 -0500

    Tema 1 assets

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,26 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
-      
+      h1 Fundamentos de la cocina fría y el #[em Garde Manger]
+  
+    .row.justify-content-center.align-items-center
+      .col-sm-6.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(src="@/assets/curso/tema1/1.png", alt="" data-aos="zoom-in-down")
+      .col-lg-9
+        .bg-img.bg-img--terciario.rounded-4.p-4.mb-3
+          p.mb-0 La cocina fría y el #[em Garde Manger] integran conocimientos, técnicas y criterios esenciales para la elaboración de preparaciones que requieren especial atención en la selección de ingredientes, manipulación, conservación, combinación de sabores, texturas y presentación. Su aplicación dentro de la cocina saludable permite desarrollar propuestas frescas, equilibradas y visualmente atractivas, manteniendo condiciones adecuadas de inocuidad y calidad durante todo el proceso culinario.  
+        p.mb-0 Para dominar la cocina fría, el aprendiz debe primero comprender su contexto histórico, la organización del trabajo y los principios de seguridad que la rigen. Este capítulo establece los cimientos conceptuales y operativos indispensables.
+
+    
+    Separador
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 1.1	Definición, historia y evolución de la cocina fría
+
+    p.mb-4.mb-lg-5.z-2 La cocina fría y el #[em Garde Manger] integran conocimientos, técnicas y criterios esenciales para la elaboración de preparaciones que requieren especial atención en la selección de ingredientes, manipulación, conservación, combinación de sabores, texturas y presentación. Su aplicación dentro de la cocina saludable permite desarrollar propuestas frescas, equilibradas y visualmente atractivas, manteniendo condiciones adecuadas de inocuidad y calidad durante todo el proceso culinario.  
+    
+    
+    
 
 </template>
 
